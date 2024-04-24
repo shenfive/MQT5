4deee9345d1bcabbb723cbb6f0f612b1119345d5
Danny Shen <shenfive@gmail.com>
2024-04-24 17:55:42 +0800

    Initial Commit

---
+++ MQT5/MQT5.h
@@ -0,0 +1,18 @@
+//
+//  MQT5.h
+//  MQT5
+//
+//  Created by 申潤五 on 2024/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MQT5.
+FOUNDATION_EXPORT double MQT5VersionNumber;
+
+//! Project version string for MQT5.
+FOUNDATION_EXPORT const unsigned char MQT5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MQT5/PublicHeader.h>
+
+
